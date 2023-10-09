0708fb76ca337ffb290bd03c761d92805a40bf16
Daryl Agustin <darylagustin@icloud.com>
2023-10-09 08:56:03 +0800

    Fixed CategoryResponseDTO typo

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct GroceryCtegoryResponseDTO: Codable {
+public struct GroceryCategoryResponseDTO: Codable {
     
    public let id: UUID
    public let title: String
