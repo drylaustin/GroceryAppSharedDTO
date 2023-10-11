e0844828989eb24399e9b30011480be2e9c52d87
Daryl Agustin <darylagustin@icloud.com>
2023-10-11 12:28:13 +0800

    Added DTOs for Grocery Items

---
+++ Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by DARYL AGUSTIN on 10/11/23.
+//
+
+import Foundation
+
+public struct GroceryItemRequestDTO: Codable {
+
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(title: String, price: Double, quantity: Int) {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+    
+}
+
